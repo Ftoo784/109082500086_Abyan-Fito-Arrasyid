278ea875bb35f973a5121da9fffa351f1eb31265
Ftoo784 <abyanfito@gmail.com>
2026-04-10 06:50:53 +0700

    Update Soal1.go

--- Modul5/Soal1.go
+++ Modul5/Soal1.go
@@ -1,17 +1,18 @@
-package main 
+package main
 import "fmt"
-func main () {
-	var x int
-	fmt.Scan(&x)
-
-	for i:=0; i<=x; i++ {
-		fmt.Printf("%d ",fibonaci(i))
+func bintang (x int){
+	if x!=0 {
+		fmt.Print("*")
+		bintang(x-1)
 	}
 }
 
-func fibonaci(n int)int{
-	if x <= 1 {
-		return x
+func main (){
+	var y int
+	fmt.Scan(&y)
+
+	for i:= 1;i<=y;i++{
+		bintang(i)
+		fmt.Println()
 	}
-	return fibonaci(x-1) + fibonaci(x-2)
 }
